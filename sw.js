40011ad132a4bf289bd69afdd4e61973d945d01b
vg135575-oss <vg135575@gmail.com>
2026-01-01 17:06:50 -0400

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("ifro-app").then(cache =>
+      cache.addAll(["index.html"])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
